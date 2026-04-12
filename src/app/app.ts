76769ec17fd1091d4bb7bf81d82f2aeb5a1f0abe
John <johneliud4@gmail.com>
2026-04-13 01:52:38 +0300

    refactor: move App component template to external HTML file for cleaner structure.

--- src/app/app.ts
+++ src/app/app.ts
@@ -6,14 +6,7 @@ import { FooterComponent } from './shared/components/footer/footer';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, NavbarComponent, FooterComponent],
-  template: `
-    <a href="#main-content" class="skip-to-content">Skip to main content</a>
-    <app-navbar />
-    <main id="main-content" tabindex="-1" class="flex-1">
-      <router-outlet />
-    </main>
-    <app-footer />
-  `,
+  templateUrl: './app.html',
   host: { class: 'flex flex-col min-h-screen' },
   changeDetection: ChangeDetectionStrategy.OnPush,
 })

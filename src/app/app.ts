6bba11c02ea2bf7ec2978fff066c11290157ec54
John <johneliud4@gmail.com>
2026-04-13 01:07:57 +0300

    refactor: simplify App component, add navbar and skip-to-content link, switch to OnPush strategy.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,17 @@
-import { Component, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './shared/components/navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  imports: [RouterOutlet, NavbarComponent],
+  template: `
+    <a href="#main-content" class="skip-to-content">Skip to main content</a>
+    <app-navbar />
+    <main id="main-content" tabindex="-1">
+      <router-outlet />
+    </main>
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class App {
-  protected readonly title = signal('kisumu-ruby-community-website');
-}
+export class App {}

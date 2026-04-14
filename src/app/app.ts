f2b78bad8080c23c6fd25229c6e8b7c23faaad17
John <johneliud4@gmail.com>
2026-04-14 09:01:32 +0300

    feat: import and add notification component to root app for global message handling.

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './shared/components/navbar/navbar';
 import { FooterComponent } from './shared/components/footer/footer';
+import { NotificationComponent } from './shared/components/notification/notification';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent, FooterComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, NotificationComponent],
   templateUrl: './app.html',
   host: { class: 'flex flex-col min-h-screen' },
   changeDetection: ChangeDetectionStrategy.OnPush,
